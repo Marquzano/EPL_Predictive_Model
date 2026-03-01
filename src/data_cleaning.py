f1704594ad36ea1eeec61151b12977d0be5363fb
Marquzano <angel.angelm.marquez3@gmail.com>
2026-03-01 15:55:41 -0500

    Setting up Data Cleaning script

---
+++ src/data_cleaning.py
@@ -0,0 +1,28 @@
+import sqlite3
+import pandas as pd
+
+csv_file = '/home/marquzano/workspace/EPL_Predictive_Model/data/raw/final_matches.csv'
+
+# function to manage SQL connection
+def sql_con():
+    con = sqlite3.connect('EPL_data.db')
+    return con
+
+# create the connection
+con = sql_con()
+
+# open the csv file as a dataframe
+matches_df = pd.read_csv(csv_file, na_filter=True)
+
+# load the dataframe into SQLite
+matches_df.to_sql('match_data', con=con, if_exists='replace', index=False)
+
+# Sample code to test connection to DB
+# sample_query = 'SELECT distinct team FROM match_data'
+# transformed_df = pd.read_sql(sample_query, con=con)
+# print(transformed_df)
+
+# Go column by column checking for NULL values
+# May need to dump entire columns
+def fill_null(con, matches_df):
+    return None
