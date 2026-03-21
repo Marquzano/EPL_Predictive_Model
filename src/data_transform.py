36401fb548105519a217031779b72ffaa7a14d27
Marquzano <angel.angelm.marquez3@gmail.com>
2026-03-21 12:59:18 -0400

    Completed the following: Added comments to go over logic Cleaned merged_matches to make desired dataset Created new csv and table enamed the old csv and old table

--- src/data_transform.py
+++ src/data_transform.py
@@ -18,17 +18,46 @@ def open_con(db_path, csv_path, table_name):
 def make_matches(df):
     id_cols = ['season', 'date', 'time', 'round']
 
+    # splitting up df into home and away matches to be merged into unique match rows
     home_df = df[df['venue'] == 'Home'].copy()
     away_df = df[df['venue'] == 'Away'].copy()
 
+    # renaming columns accordingly to later clean and consolidate
     home_df = home_df.rename(columns={c: f"{c}_home" for c in home_df.columns if c not in id_cols})
     away_df = away_df.rename(columns={c: f"{c}_away" for c in away_df.columns if c not in id_cols})
 
+    # merging into one df
     merged_df = pd.merge(home_df, away_df, on=id_cols)
 
-    print(merged_df.head()) 
+    # removing inconsistencies where matches merge on the same columns but are not the same match
+    merged_df = merged_df[merged_df['opponent_home'] == merged_df['team_away']]
+
+    # renaming columns team_home and team_away for simplicity
+    merged_df = merged_df.rename(columns={'team_home': 'home', 'team_away':'away'})
+
+    # below I'm making a single column to hold the result for each unique match
+    data = []
+
+    for value in merged_df['result_home']:
+        if value == 'W':
+            data.append('Home')
+        elif value == 'L':
+            data.append('Away')
+        else:
+            data.append('Draw')
+    
+    result = pd.Series(data)
+    merged_df['result'] = result.values
 
     return merged_df
+
+def clean_matches(con):
+    # selecting the needed features and omitting others
+    # also ordering the matches in chronological order
+    query = 'SELECT season, date, time, round, gf_home, xg_home, poss_home, sh_home, sot_home, home, gf_away, xg_away, poss_away, sh_away, sot_away, away, result FROM old_merged_matches ORDER BY date, time, round;'
+    df = pd.read_sql(query, con=con)
+
+    return df
 
 if __name__ == '__main__':
     clean_csv = 'data/processed/clean_final_matches.csv'
@@ -43,12 +72,18 @@ if __name__ == '__main__':
     # season
     merged_df = make_matches(clean_df)
 
-    merged_df.to_csv('data/processed/merged_matches.csv')
+    merged_df.to_csv('data/processed/old_merged_matches.csv')
 
-    merged_df.to_sql('merged_matches', con=con, if_exists='replace', index=False)
+    merged_df.to_sql('old_merged_matches', con=con, if_exists='replace', index=False)
 
     # need to remove some columns i.e. venue?, unnamed 0, opponent
     # need to rename team to home in home_df and away in away_df
     # number of other things need to be changed
     # getting closer
     # and learning more each step I take
+
+    clean_merged_df = clean_matches(con)
+
+    clean_merged_df.to_csv('data/processed/merged_matches.csv')
+
+    clean_merged_df.to_sql('merged_matches', con=con, if_exists='replace', index=False)
